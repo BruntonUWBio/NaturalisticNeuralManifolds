e982920fce346c8df9a783bf7cec81a89631ade7
zoeis52 <zsteineh@cs.washington.edu>
2023-09-13 10:18:33 -0700

    re-running results after bug

---
+++ run_nat_results.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Running NAT results"
+echo "-------------------"
+
+nat_ecog_file=$1
+nat_pose_file=$2
+
+echo "Running NAT results with ECoG as: $nat_ecog_file"
+echo "Running NAT results with Pose as: $nat_pose_file"
+
+# should I include TME eventually?
+
+python run_pca.py $nat_ecog_file
+python run_pca_bootstrap.py $nat_ecog_file
+python run_paa.py $nat_ecog_file
+
+python run_movement_correl.py $nat_pose_file $nat_ecog_file
+
+# runipy -o figures/CrossMovement.ipynb
+# runipy -o figures/CrossDays.ipynb
+
+python make_roi_region_contribs_df.py
+# runipy -o figures/CrossParticipants.ipynb
